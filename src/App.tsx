2247267c6561af80a5e870997001620910641156
Locky <43171368+LockyBounty@users.noreply.github.com>
2021-02-11 13:25:06 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Footer from "./Footer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Landpage</h1>
+      <h2>Pizza on the way !</h2>
+      <Footer />
+    </div>
+  );
+}
