4c5c28f46c8731807f48a074f3bee88b5c8e48c4
Locky <43171368+LockyBounty@users.noreply.github.com>
2021-02-15 13:06:43 +0100

    added - landpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./styles.css";
 import Footer from "./Footer";
+import LandPage from "./Landpage";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Landpage</h1>
-      <h2>Pizza on the way !</h2>
+      <LandPage />
       <Footer />
     </div>
   );
