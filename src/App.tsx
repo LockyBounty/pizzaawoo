2b9fc4559e4d9f67485c1e62e7c15c134d16003e
Locky <43171368+LockyBounty@users.noreply.github.com>
2021-02-22 15:41:25 +0100

    added - lines subscription

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./styles.css";
 import Footer from "./Footer";
 import LandPage from "./Landpage";
+import Subscription from "./Subscription";
 
 export default function App() {
   return (
     <div className="App">
-      <LandPage />
+      <Subscription />
       <Footer />
     </div>
   );
