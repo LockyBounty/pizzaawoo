2fdd8de7e5e460e26bc694e4dc7eb5ef751593c3
lockybounty <bounthienglocky@gmail.com>
2021-03-15 03:00:23 +0100

    added - data

---
+++ src/Recipe.tsx
@@ -0,0 +1,61 @@
+
+const Pizzas = [ 
+            {
+                "nom": "Margarita",
+                "recette":[
+                    "Sauce Tomate",
+                    "Mozzarella","Basilic"],
+                "prix":8
+            },
+            {   "nom": "Quatre Fromages",
+                "recette":[
+                    "Sauce Tomate", "Gorgonzola",
+                    "Mozzarella","Feta"],
+                "prix":10
+            },
+            {   "nom": "Quatre Saisons",
+                "recette":[
+                    "Sauce Tomate", "Oignon", "Poivron",
+                    "Jambon", "Mozzarella","Basilic"],
+                "prix":14
+            },
+            {   "nom": "Barbecue",
+                "recette":[
+                    "Sauce Tomate", "Oignon", 
+                    "Boulettes de boeuf",
+                    "Mozzarella","Basilic",
+                    "Sauce BBQ","Bacon"],
+                "prix":11
+            },
+            {   "nom": "Pepperoni",
+                "recette":[
+                    "Sauce Tomate", "Oignon",
+                    "Mozzarella","Pepperoni"],
+                "prix":12
+            },
+            {   "nom": "Vegetarien",
+                "recette":[
+                    "Sauce Tomate", "Oignon", 
+                    "Poivron","Olive","Champignon",
+                    "Mozzarella","Basilic"],
+                "prix":9
+            }
+]
+
+
+const Topping = 
+[
+    "Anchois", "Boulettes de boeuf","Thym", "Pepperoni", "Poivron", "Parmagiano Reggiano","Olive"
+
+];
+
+const Drink = 
+[
+    "A&W root", "Coca Cola", "Coca Cola Zero", "Fanta", "Dr. Pepper", "Pepsi"
+]
+
+export {
+    Pizzas, 
+    Topping,
+    Drink
+};
