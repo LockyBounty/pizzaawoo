9e67081577e1714d2920d527a731b6e9c9227de5
Locky <43171368+LockyBounty@users.noreply.github.com>
2021-03-24 16:41:40 +0100

    Added - header zone

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import LandPage from "./Landpage";
 //import Subscription from "./Subscription";
 import Pizza from "./Recipe";
 import Listing from "./Listing";
+import Header from "./Header";
 
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <Listing />
       {/*<Footer />*/}
     </div>
